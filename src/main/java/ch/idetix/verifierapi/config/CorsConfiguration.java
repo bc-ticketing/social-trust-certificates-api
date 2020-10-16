751153435a3a87da4c89e2c021aab9752105d9d7
Simon <simibac93@gmail.com>
2020-10-16 09:16:43 +0200

    added CORS configuration

---
+++ src/main/java/ch/idetix/verifierapi/config/CorsConfiguration.java
@@ -0,0 +1,24 @@
+package ch.idetix.verifierapi.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Slf4j
+@Configuration
+public class CorsConfiguration implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry
+                .addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("POST", "PUT", "DELETE", "GET", "OPTIONS")
+                .allowedHeaders("origin", "content-type", "accept")
+                .exposedHeaders("Vary", "Origin")
+                .allowCredentials(true)
+                .maxAge(3600);
+
+        log.info("Enabling CORS");
+    }
+}
--- src/main/java/ch/idetix/verifierapi/controller/VerifierController.java
+++ src/main/java/ch/idetix/verifierapi/controller/VerifierController.java
@@ -6,18 +6,15 @@ import ch.idetix.verifierapi.service.TwitterVerifierService;
 import ch.idetix.verifierapi.service.WebsiteVerifierService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
-@CrossOrigin(maxAge = 3600)
 @RequestMapping("/api/")
 @RestController
 @Validated
@@ -29,11 +26,10 @@ public class VerifierController {
     @Autowired
     private TwitterVerifierService twitterVerifierService;
 
-    @CrossOrigin(origins = "/**")
     @RequestMapping(value = "/website", method = GET)
     public WebsiteResponse verifyWebsite(
 //            @Pattern(regexp = "https?:\\/\\/(www\\.)?[-a-zA-Z0-9@:%._\\+~#=]{2,256}\\.[a-z]{2,4}\\b([-a-zA-Z0-9@:%_\\+.~#?&//=]*)", message = "url does not seem to be a valid url")
-            @Pattern(regexp = "https?:\\/\\/.*")
+//            @Pattern(regexp = "https?:\\/\\/.*")
             @RequestParam String url) {
         return websiteVerifierService.verifyWebsite(url);
     }
